f9861a53d5fb93c1f7144dde1c8e1e84a45183e7
yaekobbonke <yaekobbundure@gmail.com>
2022-09-11 08:02:58 -0700

    fixed

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++);
-	{
-		if (letter != 'e' && letter != 'q');
+	for (letter = 'a';letter++;)
+		{
+		if(letter != 'e' && letter != 'q')letter++;}
 		putchar(letter);
-	}
-	putchar('\n');
-	return (0);
+	
+		putchar('\n');
+		return (0);
 
 
 }
