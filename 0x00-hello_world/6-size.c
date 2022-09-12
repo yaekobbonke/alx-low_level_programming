d228ecad2e7ad500411b437538cefb401d287a9a
yaekobbonke <yaekobbundure@gmail.com>
2022-09-11 19:52:12 -0700

    intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * return : 0 (success)
+ */
+int main (void)
+{
+	char =[1_byte(s)];
+	int=[4_byte(s)];
+	long int= [4_ byte(s)];
+	long long int= [8_ byte(s)];
+	float =[4_ byte(s)];
+
+	printf("Size of a char %c, Size of an int %d, Size of a long int %d, Size of a long long int %d, Size of a float %f,\n");
+	return (0);
+}
