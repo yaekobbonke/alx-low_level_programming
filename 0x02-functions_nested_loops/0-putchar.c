4bbaf3318fe2f0c2e8685b2c921c8898053cbe4b
yaekobbonke <yaekobbundure@gmail.com>
2022-09-14 03:56:35 -0700

    fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,10 +5,17 @@
  * Return: Always 0
  */
 int main(void)
-{ 
-    
-	printf("_putchar\n");
-
+{    
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	
 	return (0);
 	
 }
