61ec25e76be481a46d82ac7404e2e72c2fff3404
yaekobbonke <yaekobbundure@gmail.com>
2022-09-15 07:36:21 -0700

    4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - prints the numbers, from 0 to 9, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+}
