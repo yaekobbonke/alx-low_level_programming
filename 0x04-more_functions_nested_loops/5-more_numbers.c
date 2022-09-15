f5b4f2b28500faab0cf1e4d7d17983efdbdc26e0
yaekobbonke <yaekobbundure@gmail.com>
2022-09-15 10:33:57 -0700

    fix

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *  prints 10 times the numbers, from 0 to 14
+ *
+ *  Return: Always 0
+ */
+int main (void)
+{ 
+       int i;
+	int j;
+
+	i= 0;
+	while (i<=10)
+	{
+		j = 0;
+		while (j<=14 )
+		{ 
+			putchar(j);
+			j++;
+			putchar('\n');
+		}
+	putchar('\n');
+	i++;
+	}
+}
