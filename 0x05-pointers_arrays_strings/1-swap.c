94bbc31a85acde97c1f6b3823f138b3e5759e189
yaekobbonke <yaekobbundure@gmail.com>
2022-09-19 16:34:46 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - check the code for main.h
+ * @a: declaration of a and paramters for the function swap_int
+ * @b: declaration of b and paramters for the function swap_int
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
