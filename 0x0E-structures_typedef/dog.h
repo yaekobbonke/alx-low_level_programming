c629d33e61a5bc56b8108a3ae3616593492acbcc
yaekobbonke <yaekobbundure@gmail.com>
2022-10-11 17:19:26 -0700

    fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,3 @@
-#ifndef DOG_H
-#define DOG_H
 /**
  * dog - declares variables
  *
@@ -11,11 +9,3 @@ struct dog
 	float age;
 	char *owner;
 };
-
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif
