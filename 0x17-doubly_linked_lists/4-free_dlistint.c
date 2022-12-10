4b6dcdeafa08bd103e214f2ddf0f89794a032b9a
yaekobbonke <yaekobbundure@gmail.com>
2022-12-09 16:31:49 -0800

    num 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+
+/**
+ * free_dlistint - free a doubly linked list
+ * @head: the head of list
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *phree;
+
+
+	while (head)
+	{
+		phree = head;
+		head = head->next;
+		free(phree);
+	}
+}
