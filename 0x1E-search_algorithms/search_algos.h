3a7ff7bf81946b64d7cfde604c5d138cf73f28f4
yaekobbonke <yaekobbundure@gmail.com>
2023-07-12 02:24:58 -0700

    header file fixed

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,8 @@
 #ifndef SEARCH_ALGOS
 #define SEARCH_ALGOS
 
-#include<stdio.h>
-int linear_search(int *array, size_t size, int value)
-int binary_search(int *array, size_t size, int value)
 
-#endif /#SEARCH_ALGOS#/
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /*SEARCH_ALGOS*/
